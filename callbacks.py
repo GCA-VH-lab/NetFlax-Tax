8e0bb27326aa6fa279137fb23bcf0a2ec5deaeaa
vedabojar <vedabojar@gmail.com>
2023-06-19 05:24:28 +0200

    works

--- callbacks.py
+++ callbacks.py
@@ -5,10 +5,12 @@ import pandas as pd
 import re
 import dash
 import requests
+import numpy as np
 
 import plotly.express as px
 from dash import html, dcc, callback, ctx, dash_table
-from dash.dependencies import Input, Output, State
+from dash.dependencies import Input, Output, State, ALL
+
 
 import dash_bio as dashbio
 from dash_bio.utils import PdbParser, create_mol3d_style
@@ -24,6 +26,8 @@ from data.data_prep import *
 from functions.a1_protein_coords import *
 from functions.a2_protein_strucuture import *
 from functions.a3_create_logos import *
+from functions.b1_table import *
+from functions.b2_sunburst_fig import *
 
 # Import aesthetetics
 from assets.color_scheme import *
@@ -73,12 +77,15 @@ def update_dropdown_options(taxa_click=None, acc_click=None, node_click=None):
             df['family'], 
             df['genus'], 
             df['taxa']]).tolist()))
+        options = sorted(options)
     elif button_id == 'accession-number':
         # Make a list of all ac
         options = list(set(pd.concat([df['at_accession'], df['t_accession']]).tolist()))
+        options = sorted(options)
     elif button_id == 'node':
         # Make a list of all nodes
         options = list(set(pd.concat([df['at_domain'], df['t_domain']]).tolist()))
+        options = sorted(options)
     else:
         # Otherwise the dropdown menu is empty
         options = []
@@ -90,24 +97,15 @@ def update_dropdown_options(taxa_click=None, acc_click=None, node_click=None):
 
 # 1. SLIDER FOR TAXONOMIC LEVEL
 
-# Create a list of paths with different number of levels
-paths = {
-    0: ['superkingdom'],
-    1: ['superkingdom', 'phylum'],
-    2: ['superkingdom', 'phylum', 'class'],
-    3: ['superkingdom', 'phylum', 'class', 'order'],
-    4: ['superkingdom', 'phylum', 'class', 'order', 'family'],
-    5: ['superkingdom', 'phylum', 'class', 'order', 'family', 'genus'],
-    6: ['superkingdom', 'phylum', 'class', 'order', 'family', 'genus', 'taxa']
-}
-
 @callback(
     Output('a1_taxonomy_sunburst', 'figure'),
-    Output('results-container', 'children'),
+    Output('table-container', 'children'),
     Input('taxonomy_level_slider', 'value'),
-    Input('search-dropdown', 'value')
+    Input('search-dropdown', 'value'),
+    Input({"type": "ta-button", "index": ALL}, "n_clicks"),
+    State({"type": "ta-button", "index": ALL}, "id")
 )
-def update_sunburst_level(level=None, search_term=None):
+def update_sunburst_level(level=None, search_term=None, button_clicks=None, button_ids=None):
     '''
     A callback to update the taxonomy wheel figure through:
         1. Search by taxonomy, accession, or node
@@ -117,12 +115,20 @@ def update_sunburst_level(level=None, search_term=None):
     Args:
         level: The taxonomic level
         search_term: The protein accession number
-    
+        button_index: 
+
     Returns:
         Returns an updated wheel figure based on the search input and
         the taxonomic level. It also updates the results output box on
         the right-hand side. This is unique for each category.
     '''
+    # -------------------------- VIEW STRUCTURE ------------------------    
+    if button_clicks:
+        for clicks, button_id in zip(button_clicks, button_ids):
+            if clicks and button_id:
+                button_name = button_id.get('index')
+                search_term = button_name.split('(')[0]
+                break
 
     # ------------------------------ SEARCH ----------------------------    
     if search_term != None:
@@ -140,34 +146,13 @@ def update_sunburst_level(level=None, search_term=None):
             elif len(filtered_df[filtered_df['t_accession'] == search_term]['taxa'].values) > 0:
                 last_ring = filtered_df[filtered_df['t_accession'] == search_term]['taxa'].values[0]
 
-            # Set the color of the matching segment
+            # Set the color based on matching or non-matching segments
             df['color'] = wedge_non_highlight
             df.loc[df['taxa'] == last_ring, 'color'] = wedge_highlight
 
-            # Create the sunburst for the whole dataset
-            fig = px.sunburst(
-                data_frame=df,
-                path=paths[6],
-                color='color',
-                color_discrete_map={
-                    'grey': wedge_non_highlight, 
-                    'blue': wedge_highlight
-                },
-                branchvalues='total'
-            )
-
-            fig.update_traces(
-                marker=dict(line=dict(color=page_background, width=0.5)),
-                hovertemplate='<b>%{label} </b> <br>Taxonomy: %{id}<br>Number of TAs: %{value}',
-            )
-            fig.update_layout(
-                plot_bgcolor=transparent_background,
-                paper_bgcolor=transparent_background
-            )
-            
-            # Prepare output
-            sunburst_fig = fig
+            # Updated dataset          
             dataset = df_netflax[df_netflax.values == search_term]
+            level = 6
 
             # Get protein logos
             fig_antitoxin, fig_toxin = create_protein_logos(search_term)
@@ -175,43 +160,71 @@ def update_sunburst_level(level=None, search_term=None):
             # Get structure for protein
             structure_data, styles, chain_sequence = visualising_protein(search_term)
 
-            return sunburst_fig, html.Div(
-                children=[
-                    dbc.Row([
-                        html.H4(f'Search results for "{search_term}"'),
+            # Get sunburst fig
+            sunburst_fig = create_sunburst_figure(df, level, 'color', default='no')
+
+            results_div = html.Div([
+                html.Div([
+                    html.Br(style={'padding':'10px'}),
+                    dbc.Container([
+                        dbc.Row(
+                            html.H6(
+                                f'Search results for "{search_term}"',
+                                style={
+                                    'margin-top': '40px',
+                                    'padding': '20px',
+                                    'background-color': accent_medium,
+                                    'border-radius': '10px',
+                                },
+                            )
+                        ),
                         dbc.Row([
                             dbc.Col([
+                                html.H6('Antitoxin'),
                                 dcc.Graph(
                                     id='antitoxin-logo', 
-                                    figure=fig_antitoxin, 
-                                    )
-                            ], width=5),
-                            dbc.Col(width=1),
+                                    figure=fig_antitoxin,
+                                    config={
+                                        'displayModeBar': False
+                                    }
+                                ),
+                            ],  width={'size': 5}),
+                            dbc.Col(width={'size': 1}), 
                             dbc.Col([
+                                html.H6('Toxin'),
                                 dcc.Graph(
                                     id='toxin-logo', 
-                                    figure=fig_toxin)
-                            ], width=5)
+                                    figure=fig_toxin,
+                                    config={
+                                        'displayModeBar': False
+                                    }
+                                ),
+                            ],  width={'size': 5}),
                         ]),
+                        dbc.Row(
+                            html.H6('Antitoxin-Toxin Structure')   
+                            ),
                         dbc.Row([
                             dbc.Col([
                                 dashbio.Molecule3dViewer(
                                     id='dashbio-default-molecule3d',
                                     modelData=structure_data,
                                     styles=styles
-                                )
-                            ]),
-                            dbc.Col([
-                                dashbio.SequenceViewer(
-                                    id='dashbio-default-sequenceviewer',
-                                    sequence=chain_sequence,
-                                    #title=f'Protein Sequence ({antitoxin}/{toxin})'
-                                )
-                            ])
-                        ])
-                    ])
-                ]
-            )  
+                                ),
+                            ],  width={'size': 10}, 
+                                style={'padding':'10px'})
+                        ]),
+                    ],  style={
+                            'background-color': page_background,
+                            'border-radius': '10px',
+                            'width': '100%', 
+                            'margin': '0 auto', 
+                        })
+                ])
+            ], style={'max-height': '90vh'})
+
+
+            return sunburst_fig, results_div  
 
         # 2. Search by node      
         elif search_term.startswith('D') or search_term.startswith('M') or search_term.startswith('Panacea'):
@@ -227,69 +240,21 @@ def update_sunburst_level(level=None, search_term=None):
             elif len(filtered_df[filtered_df['t_domain'] == search_term]['taxa'].values) > 0:
                 last_ring = filtered_df[filtered_df['t_domain'] == search_term]['taxa'].values[0]
 
-            # Set the color of all matching segments to lightblue
-            df['color'] = wedge_non_highlight
-            df.loc[df['taxa'] == last_ring, 'color'] = wedge_highlight
+            # Set the color based on matching or non-matching segments
+            df['color'] = np.where(df['taxa'].isin(filtered_df['taxa']), wedge_highlight, wedge_non_highlight)
 
-            # Create the sunburst for the whole dataset
-            fig = px.sunburst(
-                data_frame=df,
-                path=paths[6],
-                color='color',
-                color_discrete_map={
-                    'grey': wedge_non_highlight, 
-                    'blue': wedge_highlight
-                },
-                branchvalues='total'
-            )
+            # New dataset
+            level = 6
+            dataset = df[df.values == search_term]
 
-            fig.update_traces(
-                marker=dict(line=dict(color=page_background, width=0.5)),
-                hovertemplate='<b>%{label} </b> <br>Taxonomy: %{id}<br>Number of TAs: %{value}',
-            )
-            fig.update_layout(
-                plot_bgcolor=transparent_background,
-                paper_bgcolor=transparent_background
-            )
+            # Creating results
+            sunburst_fig = create_sunburst_figure(df, level, 'color', default='no')
+            table_title = f'Dataset filtered based on node: {search_term}'
+            table = create_table(dataset)
+            results_div = create_results_div(table_title, table, dataset)
 
-            sunburst_fig = fig
-            # Filter netflax_df by the search term
-            dataset = df_netflax[df_netflax.values == search_term]
-            
-            return sunburst_fig, html.Div(
-            children=[
-                dbc.Row([
-                    html.H4(f'Search results for "{search_term}"'),
-                    dbc.Row([
-                        'InfoBox'
-                    ]),
-                    dbc.Row([
-                        dbc.Col([
-
-                        ]),
-                        dbc.Col([
-
-                        ])
-                    ]),
-                    dbc.Row([
-                        "Selection data",
-                        html.Hr(),
-                        html.Div(id = 'molecule-info-container')
-                    ]),
-                    dbc.Row([
-                        'Filtered NetFlax Table Results',
-                        dash_table.DataTable(
-                            id = 'table',
-                            data = dataset.to_dict('records'),
-                            fixed_rows = {'headers': True},
-                            style_cell = {'minWidth': 95, 'maxWidth': 95, 'width': 95})
-                    ]),
-                ]),
-            ]
-        )    
+            return sunburst_fig, results_div  
  
-
-
         # 3. Search by taxonomy at any level
         else:
             df = df_netflax.copy()
@@ -321,120 +286,36 @@ def update_sunburst_level(level=None, search_term=None):
             else:
                 print(f'{search_term} does not exist')
 
-            # Create the sunburst for the filtered dataset
-            fig = px.sunburst(
-                data_frame=df,
-                path=paths[6],
-                color='color',
-                color_discrete_map={
-                    'grey': wedge_non_highlight, 
-                    'blue': wedge_highlight
-                },
-                branchvalues='total'
-            )
+            # New dataset
+            level = 6
+            dataset = df[df.values == search_term]
 
-            fig.update_traces(
-                marker=dict(line=dict(color=page_background, width=0.5)),
-                hovertemplate='<b>%{label} </b> <br>Taxonomy: %{id}<br>Number of TAs: %{value}',
-            )
-            fig.update_layout(
-                plot_bgcolor=transparent_background,
-                paper_bgcolor=transparent_background
-            )
+            # Creating results
+            sunburst_fig = create_sunburst_figure(df, level, 'color', default='no')
+            table_title = f'Dataset filtered based on {search_term}'
+            table = create_table(dataset)
+            results_div = create_results_div(table_title, table, dataset)
 
-            sunburst_fig = fig
-            # Filter netflax_df by the search term
-            dataset = df_netflax[df_netflax.values == search_term]
-            
-            return sunburst_fig, html.Div(
-            children=[
-                dbc.Row([
-                    html.H4(f'Search results for "{search_term}"'),
-                    dbc.Row([
-                        'InfoBox'
-                    ]),
-                    dbc.Row([
-                        dbc.Col([
+            return sunburst_fig, results_div  
 
-                        ]),
-                        dbc.Col([
-
-                        ])
-                    ]),
-                    dbc.Row([
-                        "Selection data",
-                        html.Hr(),
-                        html.Div(id = 'molecule-info-container')
-                    ]),
-                    dbc.Row([
-                        'Filtered NetFlax Table Results',
-                        dash_table.DataTable(
-                            id = 'table',
-                            data = dataset.to_dict('records'),
-                            fixed_rows = {'headers': True},
-                            style_cell = {'minWidth': 95, 'maxWidth': 95, 'width': 95})
-                    ]),
-                ]),
-            ]
-        )    
- 
-
-    # ------------------------------ SLIDER ----------------------------
+    # ----------------------------- DEFAULT ----------------------------
 
     # Create a sunburst chart with the selected number of levels
-    dataset = df_netflax
     if level is None:
         level = 3  # default value if level is not selected
 
-    # Creating the plot    
-    fig = sunburst_plot = px.sunburst(
-        data_frame = dataset,
-        path = paths[level],
-        color = 'superkingdom',
-        color_discrete_sequence = px.colors.qualitative.Pastel,
-    )
-    fig.update_traces(
-        marker = dict(line = dict(color = page_background, width = 0.75)),
-        hovertemplate='<b>%{label} </b> <br>Taxonomy: %{id}<br>Number of TAs: %{value}',
-    )
-    fig.update_layout(
-        plot_bgcolor = transparent_background,
-        paper_bgcolor = transparent_background)
-    
-    sunburst_fig = fig
+    # New dataset
+    dataset = df_netflax
 
-    return sunburst_fig, html.Div(
-            children=[
-                dbc.Row([
-                    html.H4(f'Search results for "{search_term}"'),
-                    dbc.Row([
-                        'InfoBox'
-                    ]),
-                    dbc.Row([
-                        dbc.Col([
+    # Creating results
+    sunburst_fig = create_sunburst_figure(dataset, level)
+    table_title = f'Complete NetFlax Dataset'
+    table = create_table(dataset)
+    results_div = create_results_div(table_title, table, dataset)
 
-                        ]),
-                        dbc.Col([
+    return sunburst_fig, results_div  
 
-                        ])
-                    ]),
-                    dbc.Row([
-                        "Selection data",
-                        html.Hr(),
-                        html.Div(id = 'molecule-info-container')
-                    ]),
-                    dbc.Row([
-                        'Filtered NetFlax Table Results',
-                        dash_table.DataTable(
-                            id = 'table',
-                            data = dataset.to_dict('records'),
-                            fixed_rows = {'headers': True},
-                            style_cell = {'minWidth': 95, 'maxWidth': 95, 'width': 95})
-                    ]),
-                ]),
-            ]
-        )    
- 
+
 
 
 
@@ -458,6 +339,17 @@ def display_molecule_info(click_info):
             html.Div(f'Residue name: {residue_name}'),
             html.Br()
         ])
+
+
+
+@callback(
+    Output("download-dataframe-csv", "data"),
+    Input("btn_csv", "n_clicks"),
+    prevent_initial_call=True,
+)
+def download_csv(n_clicks):
+    df = create_results_div[2]
+    return dcc.send_data_frame(df.to_csv, "mydf.csv", index=False)
 
 
 
